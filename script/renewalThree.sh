e7f6b22c56ff346ff5572dc77360005dcf4aea24
iandeeph <christ.lupher@gmail.com>
2016-05-25 08:31:12 +0700

    - kurang cermat,, else-nya ilang :(

--- script/renewalThree.sh
+++ script/renewalThree.sh
@@ -110,6 +110,7 @@ do
 		# echo "INSERT INTO outbox (DestinationNumber, TextDecoded, CreatorID) VALUES ('$TUKANGKETIK', '${threeNama[$numThree]} Perpanjang Paket Berhasil.. USSD REPLY :$perpanjangThree', 'BashAdmin');"| mysql -h$HOST -u$USER -p$PASSWORD sms
 		textNotifikasi[$numThree]="${threeNama[$numThree]} Perpanjang Paket Berhasil.. USSD REPLY :$perpanjangThree"
 		curl -X POST -H 'Content-type: application/json' --data '{"text": "```'"$textNotifikasi[$numThree]"'```", "channel": "'"$CHANNEL"'", "username": "'"$USERNAME"'", "icon_emoji": "'"$ICONEMOJI2"'"}' https://hooks.slack.com/services/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
+	else
 		echo "$currentTime - ${red}${threeNama[$numThree]} Gagal Perpanjang...${reset}"
 		echo "$currentTime - -------------------------------------------------------------------------------------------------------------"
 		attempt=1
