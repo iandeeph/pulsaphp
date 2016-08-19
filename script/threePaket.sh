1bf5edace39dd38beeacb53f691f6b6c5fc15cab
iandeeph <christ.lupher@gmail.com>
2016-08-19 20:04:15 +0700

    fix bug

--- script/threePaket.sh
+++ script/threePaket.sh
@@ -22,7 +22,7 @@ PASSWORD="c3rmat"
 #===============================================================================
 #THREE
 #===============================================================================
-threeResult=($(mysql dbpulsa -h$HOST -u$USER -p$PASSWORD -Bse "select namaProvider, noProvider, host, span from provider where namaProvider like 'Three%' and namaProvider not like 'ThreeAll%' order by length(namaProvider), namaProvider;"))
+threeResult=($(mysql dbpulsa -h$HOST -u$USER -p$PASSWORD -Bse "select namaProvider, noProvider, host, span from provider where namaProvider like 'ThreeAll%' order by length(namaProvider), namaProvider;"))
 cntThreeElm=4
 cntThree=${#threeResult[@]}
 threeSet=$(((cntThree+1)/cntThreeElm))
@@ -39,9 +39,10 @@ do
 	cekPaket=$(sshpass -padmin ssh -o StrictHostKeyChecking=no admin@${threeHost[$i]} -p12345 "asterisk -rx 'gsm send ussd ${threeSpan[$i]} *123*5*1*3#'")
 	echo $cekPaket
 
-	textSlack="`${threeNama[$i]} - ${threeNo[$i]} (Host : ${threeHost[$i]}, Span : ${threeSpan[$i]})` terblokir.. Pesan dari provider : $cekPaket"
+	textSlack="${threeNama[$i]} - ${threeNo[$i]} (Host : ${threeHost[$i]}, Span : ${threeSpan[$i]}) terblokir.. Pesan dari provider : $cekPaket"
 
 	blockCheck=${cekPaket:49:11}
 	if [[ "$blockCheck" == "System Busy" ]]; then
 		curl -X POST -H 'Content-type: application/json' --data '{"text": "```'"$textSlack"'```", "channel": "'"$CHANNEL"'", "username": "'"$USERNAME"'", "icon_emoji": "'"$ICONEMOJI2"'"}' https://hooks.slack.com/services/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
+	fi
 done
